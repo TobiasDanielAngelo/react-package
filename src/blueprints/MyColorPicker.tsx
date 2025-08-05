120a3e33c1a711beb81ffa9358a663afc39f30c9
Daniel Angelo Tobias <tobias.danielangelo@gmail.com>
2025-08-05 11:11:34 +0800

    "M - Remove Github import from MyColorPicker" [Major]

--- src/blueprints/MyColorPicker.tsx
+++ src/blueprints/MyColorPicker.tsx
@@ -1,5 +1,3 @@
-import { Github } from "@uiw/react-color";
-
 export const MyColorPicker = (props: {
   hidden?: boolean;
   label?: string;
@@ -20,12 +18,7 @@ export const MyColorPicker = (props: {
         value={value}
         onChange={(e) => onChangeValue?.(e.target.value)}
       />
-      <Github
-        color={value}
-        onChange={(color) => {
-          onChangeValue?.(color.hex);
-        }}
-      />
+
       <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-90 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-[85%] peer-focus:-translate-y-6">
         {label}
       </label>
